fba3f867621acf91d82725682602f7b3db9ffef1
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2018-08-25 20:20:11 +0530

    got 30 points for queens attack 2

--- implementation/queens-attack-ll.py
+++ implementation/queens-attack-ll.py
@@ -89,72 +89,83 @@ def queensAttack(n, k, r_q, c_q, obstacles):
 	#right
 	if r_obs_r > 0:
 		ans += r_obs_c - c_q - 1
+		# print(ans)
 
 	else:
 		ans += n - c_q
-	
+		# print(ans)
+
+
 	#left
 	if l_obs_r > 0:
 		ans += c_q - l_obs_c - 1
+		# print(ans)
+
 
 	else:
 		ans += c_q - 1
-	
+		# print(ans)
+
+
 	#top
 	if t_obs_c > 0:
 		ans += t_obs_r - r_q - 1
+		# print(ans)
+
 
 	else:
 		ans += n - r_q
-	
+		# print(ans)
+
+
 	#bottom
 	if b_obs_c > 0:
 		ans += r_q - b_obs_r - 1
+		# print(ans)
+
 
 	else:
-		ans +=r_q - b_obs_r - 1
-	
+		ans +=r_q - 1
+		# print(ans)
+
+
 	#top right
 	if rt_obs_c > 0:
-		print(rt_obs_r, ' rt_obs_r ',rt_obs_c)
-		ans += rt_obs_r - r_q - 2
+		# print(rt_obs_r, ' rt_obs_r ',rt_obs_c)
+		ans += rt_obs_r - r_q - 1
 
 	else:
-		ans += min(n - r_q - 1, n - c_q - 1 )
-		print(rt_obs_r, ' rt_obs_r ',min(n - r_q, n - c_q - 1 ))
+		ans += min(n - r_q, n - c_q)
+		# print(rt_obs_r, ' rt_obs_r ',min(n - r_q, n - c_q - 1 ))
 	
 	#top left
 	if lt_obs_c > 0:
-		print(lt_obs_r, ' lt_obs_r ',lt_obs_c)
+		# print(lt_obs_r, ' lt_obs_r ',lt_obs_c)
 		ans += lt_obs_r - r_q - 1
 
 	else:
-		ans += min(n - r_q, c_q - 1 )
-		print(lt_obs_r, ' lt_obs_r ', min(n - r_q, c_q - 1 ))
+		ans += min(n - r_q, c_q )
+		# print(lt_obs_r, ' lt_obs_r ', min(n - r_q, c_q - 1 ))
 	
 	#bottom right
 	if rb_obs_c > 0:
-		print(rb_obs_r, ' rb_obs_r ',rb_obs_c)
+		# print(rb_obs_r, ' rb_obs_r ',rb_obs_c)
 		ans += r_q - rb_obs_r - 1
 
 	else:
 		ans += min(r_q - 1, n - c_q)
-		print(rb_obs_r, ' rb_obs_r ', min(r_q - 1, n - c_q))
+		# print(rb_obs_r, ' rb_obs_r ', min(r_q - 1, n - c_q))
 	
 	#bottom left
 	if lb_obs_c > 0:
-		print(lb_obs_r, ' lb_obs_r ', lb_obs_c)
+		# print(lb_obs_r, ' lb_obs_r ', lb_obs_c)
 		ans += r_q - lb_obs_r - 1
 
 	else:
 		ans += min(r_q - 1, c_q - 1)
-		print(lb_obs_r, ' lb_obs_r ', min(r_q - 1, c_q - 1))
-	
-	#
-	#
+		# print(lb_obs_r, ' lb_obs_r ', min(r_q - 1, c_q - 1))
 
 	return ans
-
 
 
 if __name__ == '__main__':
